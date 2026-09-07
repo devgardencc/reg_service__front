1a509cbc1b27c89ce0f9f642fe8e35388dbefd48
yakov <yakovglee@gmail.com>
2026-09-07 23:01:53 +0900

    add: all uni

--- src/data.tsx
+++ src/data.tsx
@@ -769,15 +769,6 @@ export const koreanUniversities = [
     ),
   },
   {
-    value: "연세대학교",
-    label: (
-      <span>
-        <b>연세대학교</b>
-        <small>Yonsei University</small>
-      </span>
-    ),
-  },
-  {
     value: "고려대학교",
     label: (
       <span>
@@ -787,20 +778,11 @@ export const koreanUniversities = [
     ),
   },
   {
-    value: "한국과학기술원",
+    value: "연세대학교",
     label: (
       <span>
-        <b>한국과학기술원</b>
-        <small>KAIST</small>
-      </span>
-    ),
-  },
-  {
-    value: "포항공과대학교",
-    label: (
-      <span>
-        <b>포항공과대학교</b>
-        <small>POSTECH</small>
+        <b>연세대학교</b>
+        <small>Yonsei University</small>
       </span>
     ),
   },
@@ -823,11 +805,857 @@ export const koreanUniversities = [
     ),
   },
   {
+    value: "서강대학교",
+    label: (
+      <span>
+        <b>서강대학교</b>
+        <small>Sogang University</small>
+      </span>
+    ),
+  },
+  {
+    value: "중앙대학교",
+    label: (
+      <span>
+        <b>중앙대학교</b>
+        <small>Chung-Ang University</small>
+      </span>
+    ),
+  },
+  {
+    value: "경희대학교",
+    label: (
+      <span>
+        <b>경희대학교</b>
+        <small>Kyung Hee University</small>
+      </span>
+    ),
+  },
+  {
+    value: "한국외국어대학교",
+    label: (
+      <span>
+        <b>한국외국어대학교</b>
+        <small>Hankuk University of Foreign Studies</small>
+      </span>
+    ),
+  },
+  {
+    value: "서울시립대학교",
+    label: (
+      <span>
+        <b>서울시립대학교</b>
+        <small>University of Seoul</small>
+      </span>
+    ),
+  },
+  {
+    value: "건국대학교",
+    label: (
+      <span>
+        <b>건국대학교</b>
+        <small>Konkuk University</small>
+      </span>
+    ),
+  },
+  {
+    value: "동국대학교",
+    label: (
+      <span>
+        <b>동국대학교</b>
+        <small>Dongguk University</small>
+      </span>
+    ),
+  },
+  {
+    value: "홍익대학교",
+    label: (
+      <span>
+        <b>홍익대학교</b>
+        <small>Hongik University</small>
+      </span>
+    ),
+  },
+  {
+    value: "국민대학교",
+    label: (
+      <span>
+        <b>국민대학교</b>
+        <small>Kookmin University</small>
+      </span>
+    ),
+  },
+  {
+    value: "숭실대학교",
+    label: (
+      <span>
+        <b>숭실대학교</b>
+        <small>Soongsil University</small>
+      </span>
+    ),
+  },
+  {
+    value: "세종대학교",
+    label: (
+      <span>
+        <b>세종대학교</b>
+        <small>Sejong University</small>
+      </span>
+    ),
+  },
+  {
+    value: "단국대학교",
+    label: (
+      <span>
+        <b>단국대학교</b>
+        <small>Dankook University</small>
+      </span>
+    ),
+  },
+  {
+    value: "명지대학교",
+    label: (
+      <span>
+        <b>명지대학교</b>
+        <small>Myongji University</small>
+      </span>
+    ),
+  },
+  {
+    value: "상명대학교",
+    label: (
+      <span>
+        <b>상명대학교</b>
+        <small>Sangmyung University</small>
+      </span>
+    ),
+  },
+  {
+    value: "광운대학교",
+    label: (
+      <span>
+        <b>광운대학교</b>
+        <small>Kwangwoon University</small>
+      </span>
+    ),
+  },
+  {
+    value: "서경대학교",
+    label: (
+      <span>
+        <b>서경대학교</b>
+        <small>Seokyeong University</small>
+      </span>
+    ),
+  },
+  {
+    value: "삼육대학교",
+    label: (
+      <span>
+        <b>삼육대학교</b>
+        <small>Sahmyook University</small>
+      </span>
+    ),
+  },
+  {
+    value: "한성대학교",
+    label: (
+      <span>
+        <b>한성대학교</b>
+        <small>Hansung University</small>
+      </span>
+    ),
+  },
+  {
+    value: "서울과학기술대학교",
+    label: (
+      <span>
+        <b>서울과학기술대학교</b>
+        <small>Seoul National University of Science and Technology</small>
+      </span>
+    ),
+  },
+  {
     value: "이화여자대학교",
     label: (
       <span>
         <b>이화여자대학교</b>
         <small>Ewha Womans University</small>
+      </span>
+    ),
+  },
+  {
+    value: "숙명여자대학교",
+    label: (
+      <span>
+        <b>숙명여자대학교</b>
+        <small>Sookmyung Women's University</small>
+</span>
+    ),
+  },
+  {
+    value: "성신여자대학교",
+    label: (
+      <span>
+        <b>성신여자대학교</b>
+        <small>Sungshin Women's University</small>
+      </span>
+    ),
+  },
+  {
+    value: "서울여자대학교",
+    label: (
+      <span>
+        <b>서울여자대학교</b>
+        <small>Seoul Women's University</small>
+      </span>
+    ),
+  },
+  {
+    value: "동덕여자대학교",
+    label: (
+      <span>
+        <b>동덕여자대학교</b>
+        <small>Dongduk Women's University</small>
+      </span>
+    ),
+  },
+  {
+    value: "덕성여자대학교",
+    label: (
+      <span>
+        <b>덕성여자대학교</b>
+        <small>Duksung Women's University</small>
+      </span>
+    ),
+  },
+  {
+    value: "한국과학기술원 (KAIST)",
+    label: (
+      <span>
+        <b>한국과학기술원 (KAIST)</b>
+        <small>KAIST</small>
+      </span>
+    ),
+  },
+  {
+    value: "포항공과대학교 (POSTECH)",
+    label: (
+      <span>
+        <b>포항공과대학교 (POSTECH)</b>
+        <small>POSTECH</small>
+      </span>
+    ),
+  },
+  {
+    value: "광주과학기술원 (GIST)",
+    label: (
+      <span>
+        <b>광주과학기술원 (GIST)</b>
+        <small>GIST</small>
+      </span>
+    ),
+  },
+  {
+    value: "대구경북과학기술원 (DGIST)",
+    label: (
+      <span>
+        <b>대구경북과학기술원 (DGIST)</b>
+        <small>DGIST</small>
+      </span>
+    ),
+  },
+  {
+    value: "울산과학기술원 (UNIST)",
+    label: (
+      <span>
+        <b>울산과학기술원 (UNIST)</b>
+        <small>UNIST</small>
+      </span>
+    ),
+  },
+  {
+    value: "한국예술종합학교 (한예종)",
+    label: (
+      <span>
+        <b>한국예술종합학교 (한예종)</b>
+        <small>Korea National University of Arts</small>
+      </span>
+    ),
+  },
+  {
+    value: "한국체육대학교",
+    label: (
+      <span>
+        <b>한국체육대학교</b>
+        <small>Korea National Sport University</small>
+      </span>
+    ),
+  },
+  {
+    value: "한국교원대학교",
+    label: (
+      <span>
+        <b>한국교원대학교</b>
+        <small>Korea National University of Education</small>
+      </span>
+    ),
+  },
+  {
+    value: "한국공학대학교",
+    label: (
+      <span>
+        <b>한국공학대학교</b>
+        <small>Tech University of Korea</small>
+      </span>
+    ),
+  },
+  {
+    value: "한국항공대학교",
+    label: (
+      <span>
+        <b>한국항공대학교</b>
+        <small>Korea Aerospace University</small>
+      </span>
+    ),
+  },
+  {
+    value: "한국해양대학교",
+    label: (
+      <span>
+        <b>한국해양대학교</b>
+        <small>Korea Maritime & Ocean University</small>
+      </span>
+    ),
+  },
+  {
+    value: "인하대학교",
+    label: (
+      <span>
+        <b>인하대학교</b>
+        <small>Inha University</small>
+      </span>
+    ),
+  },
+  {
+    value: "인천대학교",
+    label: (
+      <span>
+        <b>인천대학교</b>
+        <small>Incheon National University</small>
+      </span>
+    ),
+  },
+  {
+    value: "아주대학교",
+    label: (
+      <span>
+        <b>아주대학교</b>
+        <small>Ajou University</small>
+      </span>
+    ),
+  },
+  {
+    value: "가천대학교",
+    label: (
+      <span>
+        <b>가천대학교</b>
+        <small>Gachon University</small>
+      </span>
+    ),
+  },
+  {
+    value: "경기대학교",
+    label: (
+      <span>
+        <b>경기대학교</b>
+        <small>Kyonggi University</small>
+      </span>
+    ),
+  },
+  {
+    value: "한양대학교 ERICA",
+    label: (
+      <span>
+        <b>한양대학교 ERICA</b>
+        <small>Hanyang University ERICA</small>
+      </span>
+    ),
+  },
+  {
+    value: "한국외국어대학교 글로벌",
+    label: (
+      <span>
+        <b>한국외국어대학교 글로벌</b>
+        <small>HUFS Global Campus</small>
+      </span>
+    ),
+  },
+  {
+    value: "경희대학교 국제캠퍼스",
+    label: (
+      <span>
+        <b>경희대학교 국제캠퍼스</b>
+        <small>Kyung Hee University Global</small>
+      </span>
+    ),
+  },
+  {
+    value: "중앙대학교 다빈치캠퍼스",
+    label: (
+      <span>
+        <b>중앙대학교 다빈치캠퍼스</b>
+        <small>Chung-Ang University Davinci</small>
+      </span>
+    ),
+  },
+  {
+    value: "가톨릭대학교",
+    label: (
+      <span>
+        <b>가톨릭대학교</b>
+<small>The Catholic University of Korea</small>
+      </span>
+    ),
+  },
+  {
+    value: "차의과학대학교",
+    label: (
+      <span>
+        <b>차의과학대학교</b>
+        <small>CHA University</small>
+      </span>
+    ),
+  },
+  {
+    value: "한경국립대학교",
+    label: (
+      <span>
+        <b>한경국립대학교</b>
+        <small>Hankyong National University</small>
+      </span>
+    ),
+  },
+  {
+    value: "대진대학교",
+    label: (
+      <span>
+        <b>대진대학교</b>
+        <small>Daejin University</small>
+      </span>
+    ),
+  },
+  {
+    value: "평택대학교",
+    label: (
+      <span>
+        <b>평택대학교</b>
+        <small>Pyeongtaek University</small>
+      </span>
+    ),
+  },
+  {
+    value: "성결대학교",
+    label: (
+      <span>
+        <b>성결대학교</b>
+        <small>Sungkyul University</small>
+      </span>
+    ),
+  },
+  {
+    value: "안양대학교",
+    label: (
+      <span>
+        <b>안양대학교</b>
+        <small>Anyang University</small>
+      </span>
+    ),
+  },
+  {
+    value: "신한대학교",
+    label: (
+      <span>
+        <b>신한대학교</b>
+        <small>Shinhan University</small>
+      </span>
+    ),
+  },
+  {
+    value: "협성대학교",
+    label: (
+      <span>
+        <b>협성대학교</b>
+        <small>Hyupsung University</small>
+      </span>
+    ),
+  },
+  {
+    value: "강남대학교",
+    label: (
+      <span>
+        <b>강남대학교</b>
+        <small>Kangnam University</small>
+      </span>
+    ),
+  },
+  {
+    value: "용인대학교",
+    label: (
+      <span>
+        <b>용인대학교</b>
+        <small>Yong In University</small>
+      </span>
+    ),
+  },
+  {
+    value: "수원대학교",
+    label: (
+      <span>
+        <b>수원대학교</b>
+        <small>The University of Suwon</small>
+      </span>
+    ),
+  },
+  {
+    value: "을지대학교",
+    label: (
+      <span>
+        <b>을지대학교</b>
+        <small>Eulji University</small>
+      </span>
+    ),
+  },
+  {
+    value: "강원대학교",
+    label: (
+      <span>
+        <b>강원대학교</b>
+        <small>Kangwon National University</small>
+      </span>
+    ),
+  },
+  {
+    value: "국립강릉원주대학교",
+    label: (
+      <span>
+        <b>국립강릉원주대학교</b>
+        <small>Gangneung-Wonju National University</small>
+      </span>
+    ),
+  },
+  {
+    value: "연세대학교 미래캠퍼스",
+    label: (
+      <span>
+        <b>연세대학교 미래캠퍼스</b>
+        <small>Yonsei University Mirae</small>
+      </span>
+    ),
+  },
+  {
+    value: "한림대학교",
+    label: (
+      <span>
+        <b>한림대학교</b>
+        <small>Hallym University</small>
+      </span>
+    ),
+  },
+  {
+    value: "가톨릭관동대학교",
+    label: (
+      <span>
+        <b>가톨릭관동대학교</b>
+        <small>Catholic Kwandong University</small>
+      </span>
+    ),
+  },
+  {
+    value: "상지대학교",
+    label: (
+      <span>
+        <b>상지대학교</b>
+        <small>Sangji University</small>
+      </span>
+    ),
+  },
+  {
+    value: "충남대학교",
+    label: (
+      <span>
+        <b>충남대학교</b>
+        <small>Chungnam National University</small>
+      </span>
+    ),
+  },
+  {
+    value: "충북대학교",
+    label: (
+      <span>
+        <b>충북대학교</b>
+        <small>Chungbuk National University</small>
+      </span>
+    ),
+  },
+  {
+    value: "국립한밭대학교",
+    label: (
+      <span>
+        <b>국립한밭대학교</b>
+        <small>Hanbat National University</small>
+      </span>
+    ),
+  },
+  {
+    value: "고려대학교 세종캠퍼스",
+    label: (
+      <span>
+        <b>고려대학교 세종캠퍼스</b>
+        <small>Korea University Sejong</small>
+      </span>
+    ),
+  },
+  {
+    value: "홍익대학교 세종캠퍼스",
+    label: (
+      <span>
+        <b>홍익대학교 세종캠퍼스</b>
+        <small>Hongik University Sejong</small>
+      </span>
+    ),
+  },
+  {
+    value: "한남대학교",
+    label: (
+      <span>
+        <b>한남대학교</b>
+        <small>Hannam University</small>
+      </span>
+    ),
+  },
+  {
+    value: "목원대학교",
+    label: (
+      <span>
+        <b>목원대학교</b>
+        <small>Mokwon University</small>
+      </span>
+    ),
+  },
+  {
+    value: "배재대학교",
+    label: (
+<span>
+        <b>배재대학교</b>
+        <small>Pai Chai University</small>
+      </span>
+    ),
+  },
+  {
+    value: "우송대학교",
+    label: (
+      <span>
+        <b>우송대학교</b>
+        <small>Woosong University</small>
+      </span>
+    ),
+  },
+  {
+    value: "대전대학교",
+    label: (
+      <span>
+        <b>대전대학교</b>
+        <small>Daejeon University</small>
+      </span>
+    ),
+  },
+  {
+    value: "순천향대학교",
+    label: (
+      <span>
+        <b>순천향대학교</b>
+        <small>Soonchunhyang University</small>
+      </span>
+    ),
+  },
+  {
+    value: "호서대학교",
+    label: (
+      <span>
+        <b>호서대학교</b>
+        <small>Hoseo University</small>
+      </span>
+    ),
+  },
+  {
+    value: "선문대학교",
+    label: (
+      <span>
+        <b>선문대학교</b>
+        <small>Sun Moon University</small>
+      </span>
+    ),
+  },
+  {
+    value: "상명대학교 천안캠퍼스",
+    label: (
+      <span>
+        <b>상명대학교 천안캠퍼스</b>
+        <small>Sangmyung University Cheonan</small>
+      </span>
+    ),
+  },
+  {
+    value: "남서울대학교",
+    label: (
+      <span>
+        <b>남서울대학교</b>
+        <small>Namseoul University</small>
+      </span>
+    ),
+  },
+  {
+    value: "한국기술교육대학교",
+    label: (
+      <span>
+        <b>한국기술교육대학교</b>
+        <small>KOREATECH</small>
+      </span>
+    ),
+  },
+  {
+    value: "국립공주대학교",
+    label: (
+      <span>
+        <b>국립공주대학교</b>
+        <small>Kongju National University</small>
+      </span>
+    ),
+  },
+  {
+    value: "건국대학교 글로컬캠퍼스",
+    label: (
+      <span>
+        <b>건국대학교 글로컬캠퍼스</b>
+        <small>Konkuk University GLOCAL</small>
+      </span>
+    ),
+  },
+  {
+    value: "청주대학교",
+    label: (
+      <span>
+        <b>청주대학교</b>
+        <small>Cheongju University</small>
+      </span>
+    ),
+  },
+  {
+    value: "서원대학교",
+    label: (
+      <span>
+        <b>서원대학교</b>
+        <small>Seowon University</small>
+      </span>
+    ),
+  },
+  {
+    value: "세명대학교",
+    label: (
+      <span>
+        <b>세명대학교</b>
+        <small>Semyung University</small>
+      </span>
+    ),
+  },
+  {
+    value: "국립한국교통대학교",
+    label: (
+      <span>
+        <b>국립한국교통대학교</b>
+        <small>Korea National Transportation University</small>
+      </span>
+    ),
+  },
+  {
+    value: "경북대학교",
+    label: (
+      <span>
+        <b>경북대학교</b>
+        <small>Kyungpook National University</small>
+      </span>
+    ),
+  },
+  {
+    value: "영남대학교",
+    label: (
+      <span>
+        <b>영남대학교</b>
+        <small>Yeungnam University</small>
+      </span>
+    ),
+  },
+  {
+    value: "계명대학교",
+    label: (
+      <span>
+        <b>계명대학교</b>
+        <small>Keimyung University</small>
+      </span>
+    ),
+  },
+  {
+    value: "대구가톨릭대학교",
+    label: (
+      <span>
+        <b>대구가톨릭대학교</b>
+        <small>Daegu Catholic University</small>
+      </span>
+    ),
+  },
+  {
+    value: "대구대학교",
+    label: (
+      <span>
+        <b>대구대학교</b>
+        <small>Daegu University</small>
+      </span>
+    ),
+  },
+  {
+    value: "한동대학교",
+    label: (
+      <span>
+        <b>한동대학교</b>
+        <small>Handong Global University</small>
+      </span>
+    ),
+  },
+  {
+    value: "국립금오공과대학교",
+    label: (
+      <span>
+        <b>국립금오공과대학교</b>
+        <small>Kumoh National Institute of Technology</small>
+      </span>
+    ),
+  },
+  {
+    value: "국립안동대학교",
+    label: (
+      <span>
+        <b>국립안동대학교</b>
+        <small>National Andong University</small>
+      </span>
+    ),
+  },
+  {
+    value: "동국대학교 WISE캠퍼스",
+    label: (
+      <span>
+        <b>동국대학교 WISE캠퍼스</b>
+        <small>Dongguk University WISE</small>
       </span>
     ),
   },
@@ -841,11 +1669,218 @@ export const koreanUniversities = [
     ),
   },
   {
-    value: "광운대학교",
+    value: "국립부경대학교",
     label: (
       <span>
-        <b>광운대학교</b>
-        <small>Kwangwoon University</small>
+        <b>국립부경대학교</b>
+        <small>Pukyong National University</small>
+</span>
+    ),
+  },
+  {
+    value: "동아대학교",
+    label: (
+      <span>
+        <b>동아대학교</b>
+        <small>Dong-A University</small>
+      </span>
+    ),
+  },
+  {
+    value: "경성대학교",
+    label: (
+      <span>
+        <b>경성대학교</b>
+        <small>Kyungsung University</small>
+      </span>
+    ),
+  },
+  {
+    value: "동서대학교",
+    label: (
+      <span>
+        <b>동서대학교</b>
+        <small>Dongseo University</small>
+      </span>
+    ),
+  },
+  {
+    value: "동의대학교",
+    label: (
+      <span>
+        <b>동의대학교</b>
+        <small>Dong-Eui University</small>
+      </span>
+    ),
+  },
+  {
+    value: "신라대학교",
+    label: (
+      <span>
+        <b>신라대학교</b>
+        <small>Silla University</small>
+      </span>
+    ),
+  },
+  {
+    value: "부산외국어대학교",
+    label: (
+      <span>
+        <b>부산외국어대학교</b>
+        <small>Busan University of Foreign Studies</small>
+      </span>
+    ),
+  },
+  {
+    value: "울산대학교",
+    label: (
+      <span>
+        <b>울산대학교</b>
+        <small>University of Ulsan</small>
+      </span>
+    ),
+  },
+  {
+    value: "경상국립대학교",
+    label: (
+      <span>
+        <b>경상국립대학교</b>
+        <small>Gyeongsang National University</small>
+      </span>
+    ),
+  },
+  {
+    value: "국립창원대학교",
+    label: (
+      <span>
+        <b>국립창원대학교</b>
+        <small>Changwon National University</small>
+      </span>
+    ),
+  },
+  {
+    value: "인제대학교",
+    label: (
+      <span>
+        <b>인제대학교</b>
+        <small>Inje University</small>
+      </span>
+    ),
+  },
+  {
+    value: "경남대학교",
+    label: (
+      <span>
+        <b>경남대학교</b>
+        <small>Kyungnam University</small>
+      </span>
+    ),
+  },
+  {
+    value: "전남대학교",
+    label: (
+      <span>
+        <b>전남대학교</b>
+        <small>Chonnam National University</small>
+      </span>
+    ),
+  },
+  {
+    value: "전북대학교",
+    label: (
+      <span>
+        <b>전북대학교</b>
+        <small>Jeonbuk National University</small>
+      </span>
+    ),
+  },
+  {
+    value: "조선대학교",
+    label: (
+      <span>
+        <b>조선대학교</b>
+        <small>Chosun University</small>
+      </span>
+    ),
+  },
+  {
+    value: "호남대학교",
+    label: (
+      <span>
+        <b>호남대학교</b>
+        <small>Honam University</small>
+      </span>
+    ),
+  },
+  {
+    value: "광주대학교",
+    label: (
+      <span>
+        <b>광주대학교</b>
+        <small>Gwangju University</small>
+      </span>
+    ),
+  },
+  {
+    value: "국립목포대학교",
+    label: (
+      <span>
+        <b>국립목포대학교</b>
+        <small>Mokpo National University</small>
+      </span>
+    ),
+  },
+  {
+    value: "국립순천대학교",
+    label: (
+      <span>
+        <b>국립순천대학교</b>
+        <small>Sunchon National University</small>
+      </span>
+    ),
+  },
+  {
+    value: "원광대학교",
+    label: (
+      <span>
+        <b>원광대학교</b>
+        <small>Wonkwang University</small>
+      </span>
+    ),
+  },
+  {
+    value: "전주대학교",
+    label: (
+      <span>
+        <b>전주대학교</b>
+        <small>Jeonju University</small>
+      </span>
+    ),
+  },
+  {
+    value: "국립군산대학교",
+    label: (
+      <span>
+        <b>국립군산대학교</b>
+        <small>Kunsan National University</small>
+      </span>
+    ),
+  },
+  {
+    value: "우석대학교",
+    label: (
+      <span>
+        <b>우석대학교</b>
+        <small>Woosuk University</small>
+      </span>
+    ),
+  },
+  {
+    value: "제주대학교",
+    label: (
+      <span>
+        <b>제주대학교</b>
+        <small>Jeju National University</small>
       </span>
     ),
   },
