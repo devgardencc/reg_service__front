a2caac31a018bfbc3554a275e33ca1c5c9674c0b
yakov <yakovglee@gmail.com>
2026-09-07 23:53:52 +0900

    trigger test

--- src/data.tsx
+++ src/data.tsx
@@ -19,7 +19,7 @@ export const koreanUniversities = rawKoreanUniversities.map((item) => ({
   label: (
     <Flex justify="space-between" align="center" style={{ width: "100%" }}>
       <span style={{fontWeight: 5000}}>{item.ko}</span>
-      <span style={{ color: "#8c8c8c", fontSize: "12px", marginLeft: "8px" }}>
+      <span style={{ color: "#8c8c8c", fontSize: "14px", marginLeft: "8px" }}>
         {item.en}
       </span>
     </Flex>
