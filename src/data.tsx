eafb6672f36c8683740ef3a1249cddb2f340b299
yakov <yakovglee@gmail.com>
2026-09-07 22:49:16 +0900

    add: all cities

--- src/data.tsx
+++ src/data.tsx
@@ -63,11 +63,11 @@ export const koreanCities = [
     ),
   },
   {
-    value: "제주특별자치도",
+    value: "세종특별자치시",
     label: (
       <span>
-        <b>제주특별자치도</b>
-        <small>Jeju</small>
+        <b>세종특별자치시</b>
+        <small>Sejong</small>
       </span>
     ),
   },
@@ -86,6 +86,673 @@ export const koreanCities = [
       <span>
         <b>성남시</b>
         <small>Seongnam</small>
+      </span>
+    ),
+  },
+  {
+    value: "고양시",
+    label: (
+      <span>
+        <b>고양시</b>
+        <small>Goyang</small>
+      </span>
+    ),
+  },
+  {
+    value: "용인시",
+    label: (
+      <span>
+        <b>용인시</b>
+        <small>Yongin</small>
+      </span>
+    ),
+  },
+  {
+    value: "부천시",
+    label: (
+      <span>
+        <b>부천시</b>
+        <small>Bucheon</small>
+      </span>
+    ),
+  },
+  {
+    value: "안산시",
+    label: (
+      <span>
+        <b>안산시</b>
+        <small>Ansan</small>
+      </span>
+    ),
+  },
+  {
+    value: "안양시",
+    label: (
+      <span>
+        <b>안양시</b>
+        <small>Anyang</small>
+      </span>
+    ),
+  },
+  {
+    value: "남양주시",
+    label: (
+      <span>
+        <b>남양주시</b>
+        <small>Namyangju</small>
+      </span>
+    ),
+  },
+  {
+    value: "화성시",
+    label: (
+      <span>
+        <b>화성시</b>
+        <small>Hwaseong</small>
+      </span>
+    ),
+  },
+  {
+    value: "평택시",
+    label: (
+      <span>
+        <b>평택시</b>
+        <small>Pyeongtaek</small>
+      </span>
+    ),
+  },
+  {
+    value: "의정부시",
+    label: (
+      <span>
+        <b>의정부시</b>
+        <small>Uijeongbu</small>
+      </span>
+    ),
+  },
+  {
+    value: "시흥시",
+    label: (
+      <span>
+        <b>시흥시</b>
+        <small>Siheung</small>
+      </span>
+    ),
+  },
+  {
+    value: "파주시",
+    label: (
+      <span>
+        <b>파주시</b>
+        <small>Paju</small>
+      </span>
+    ),
+  },
+  {
+    value: "김포시",
+    label: (
+      <span>
+        <b>김포시</b>
+        <small>Gimpo</small>
+      </span>
+    ),
+  },
+  {
+    value: "광명시",
+    label: (
+      <span>
+        <b>광명시</b>
+        <small>Gwangmyeong</small>
+      </span>
+    ),
+  },
+  {
+    value: "군포시",
+    label: (
+      <span>
+        <b>군포시</b>
+        <small>Gunpo</small>
+      </span>
+    ),
+  },
+  {
+    value: "오산시",
+    label: (
+      <span>
+        <b>오산시</b>
+        <small>Osan</small>
+      </span>
+    ),
+  },
+  {
+    value: "이천시",
+    label: (
+      <span>
+        <b>이천시</b>
+        <small>Icheon</small>
+      </span>
+    ),
+  },
+  {
+    value: "양주시",
+    label: (
+      <span>
+        <b>양주시</b>
+        <small>Yangju</small>
+      </span>
+    ),
+  },
+  {
+    value: "안성시",
+    label: (
+      <span>
+        <b>안성시</b>
+        <small>Anseong</small>
+      </span>
+    ),
+  },
+  {
+    value: "구리시",
+    label: (
+      <span>
+        <b>구리시</b>
+        <small>Guri</small>
+      </span>
+    ),
+  },
+    {
+    value: "포천시",
+    label: (
+<span>
+        <b>포천시</b>
+        <small>Pocheon</small>
+      </span>
+    ),
+  },
+  {
+    value: "의왕시",
+    label: (
+      <span>
+        <b>의왕시</b>
+        <small>Uiwang</small>
+      </span>
+    ),
+  },
+  {
+    value: "하남시",
+    label: (
+      <span>
+        <b>하남시</b>
+        <small>Hanam</small>
+      </span>
+    ),
+  },
+  {
+    value: "여주시",
+    label: (
+      <span>
+        <b>여주시</b>
+        <small>Yeoju</small>
+      </span>
+    ),
+  },
+  {
+    value: "동두천시",
+    label: (
+      <span>
+        <b>동두천시</b>
+        <small>Dongducheon</small>
+      </span>
+    ),
+  },
+  {
+    value: "과천시",
+    label: (
+      <span>
+        <b>과천시</b>
+        <small>Gwacheon</small>
+      </span>
+    ),
+  },
+  {
+    value: "춘천시",
+    label: (
+      <span>
+        <b>춘천시</b>
+        <small>Chuncheon</small>
+      </span>
+    ),
+  },
+  {
+    value: "원주시",
+    label: (
+      <span>
+        <b>원주시</b>
+        <small>Wonju</small>
+      </span>
+    ),
+  },
+  {
+    value: "강릉시",
+    label: (
+      <span>
+        <b>강릉시</b>
+        <small>Gangneung</small>
+      </span>
+    ),
+  },
+  {
+    value: "동해시",
+    label: (
+      <span>
+        <b>동해시</b>
+        <small>Donghae</small>
+      </span>
+    ),
+  },
+  {
+    value: "태백시",
+    label: (
+      <span>
+        <b>태백시</b>
+        <small>Taebaek</small>
+      </span>
+    ),
+  },
+  {
+    value: "속초시",
+    label: (
+      <span>
+        <b>속초시</b>
+        <small>Sokcho</small>
+      </span>
+    ),
+  },
+  {
+    value: "삼척시",
+    label: (
+      <span>
+        <b>삼척시</b>
+        <small>Samcheok</small>
+      </span>
+    ),
+  },
+  {
+    value: "청주시",
+    label: (
+      <span>
+        <b>청주시</b>
+        <small>Cheongju</small>
+      </span>
+    ),
+  },
+  {
+    value: "충주시",
+    label: (
+      <span>
+        <b>충주시</b>
+        <small>Chungju</small>
+      </span>
+    ),
+  },
+  {
+    value: "제천시",
+    label: (
+      <span>
+        <b>제천시</b>
+        <small>Jecheon</small>
+      </span>
+    ),
+  },
+  {
+    value: "천안시",
+    label: (
+      <span>
+        <b>천안시</b>
+        <small>Cheonan</small>
+      </span>
+    ),
+  },
+  {
+    value: "공주시",
+    label: (
+      <span>
+        <b>공주시</b>
+        <small>Gongju</small>
+      </span>
+    ),
+  },
+  {
+    value: "보령시",
+    label: (
+      <span>
+        <b>보령시</b>
+        <small>Boryeong</small>
+      </span>
+    ),
+  },
+  {
+    value: "아산시",
+    label: (
+      <span>
+        <b>아산시</b>
+        <small>Asan</small>
+      </span>
+    ),
+  },
+  {
+    value: "서산시",
+    label: (
+      <span>
+        <b>서산시</b>
+        <small>Seosan</small>
+      </span>
+    ),
+  },
+  {
+    value: "논산시",
+    label: (
+      <span>
+        <b>논산시</b>
+        <small>Nonsan</small>
+      </span>
+    ),
+  },
+  {
+    value: "계룡시",
+    label: (
+      <span>
+        <b>계룡시</b>
+        <small>Gyeryong</small>
+      </span>
+    ),
+  },
+  {
+    value: "당진시",
+    label: (
+      <span>
+        <b>당진시</b>
+        <small>Dangjin</small>
+      </span>
+    ),
+  },
+  {
+    value: "전주시",
+    label: (
+      <span>
+        <b>전주시</b>
+        <small>Jeonju</small>
+      </span>
+    ),
+  },
+  {
+    value: "군산시",
+    label: (
+      <span>
+        <b>군산시</b>
+        <small>Gunsan</small>
+      </span>
+    ),
+  },
+  {
+    value: "익산시",
+    label: (
+      <span>
+        <b>익산시</b>
+        <small>Iksan</small>
+      </span>
+    ),
+  },
+  {
+    value: "정읍시",
+    label: (
+      <span>
+        <b>정읍시</b>
+        <small>Jeongeup</small>
+      </span>
+    ),
+  },
+  {
+    value: "남원시",
+    label: (
+      <span>
+        <b>남원시</b>
+        <small>Namwon</small>
+      </span>
+    ),
+  },
+  {
+    value: "김제시",
+    label: (
+      <span>
+        <b>김제시</b>
+        <small>Gimje</small>
+      </span>
+    ),
+  },
+  {
+    value: "목포시",
+    label: (
+      <span>
+        <b>목포시</b>
+        <small>Mokpo</small>
+      </span>
+    ),
+
+},
+  {
+value: "여수시",
+    label: (
+      <span>
+        <b>여수시</b>
+        <small>Yeosu</small>
+      </span>
+    ),
+  },
+  {
+    value: "순천시",
+    label: (
+      <span>
+        <b>순천시</b>
+        <small>Suncheon</small>
+      </span>
+    ),
+  },
+  {
+    value: "나주시",
+    label: (
+      <span>
+        <b>나주시</b>
+        <small>Naju</small>
+      </span>
+    ),
+  },
+  {
+    value: "광양시",
+    label: (
+      <span>
+        <b>광양시</b>
+        <small>Gwangyang</small>
+      </span>
+    ),
+  },
+  {
+    value: "포항시",
+    label: (
+      <span>
+        <b>포항시</b>
+        <small>Pohang</small>
+      </span>
+    ),
+  },
+  {
+    value: "경주시",
+    label: (
+      <span>
+        <b>경주시</b>
+        <small>Gyeongju</small>
+      </span>
+    ),
+  },
+  {
+    value: "김천시",
+    label: (
+      <span>
+        <b>김천시</b>
+        <small>Gimcheon</small>
+      </span>
+    ),
+  },
+  {
+    value: "안동시",
+    label: (
+      <span>
+        <b>안동시</b>
+        <small>Andong</small>
+      </span>
+    ),
+  },
+  {
+    value: "구미시",
+    label: (
+      <span>
+        <b>구미시</b>
+        <small>Gumi</small>
+      </span>
+    ),
+  },
+  {
+    value: "영주시",
+    label: (
+      <span>
+        <b>영주시</b>
+        <small>Yeongju</small>
+      </span>
+    ),
+  },
+  {
+    value: "영천시",
+    label: (
+      <span>
+        <b>영천시</b>
+        <small>Yeongcheon</small>
+      </span>
+    ),
+  },
+  {
+    value: "상주시",
+    label: (
+      <span>
+        <b>상주시</b>
+        <small>Sangju</small>
+      </span>
+    ),
+  },
+  {
+    value: "문경시",
+    label: (
+      <span>
+        <b>문경시</b>
+        <small>Mungyeong</small>
+      </span>
+    ),
+  },
+  {
+    value: "경산시",
+    label: (
+      <span>
+        <b>경산시</b>
+        <small>Gyeongsan</small>
+      </span>
+    ),
+  },
+  {
+    value: "창원시",
+    label: (
+      <span>
+        <b>창원시</b>
+        <small>Changwon</small>
+      </span>
+    ),
+  },
+  {
+    value: "진주시",
+    label: (
+      <span>
+        <b>진주시</b>
+        <small>Jinju</small>
+      </span>
+    ),
+  },
+  {
+    value: "통영시",
+    label: (
+      <span>
+        <b>통영시</b>
+        <small>Tongyeong</small>
+      </span>
+    ),
+  },
+  {
+    value: "사천시",
+    label: (
+      <span>
+        <b>사천시</b>
+        <small>Sacheon</small>
+      </span>
+    ),
+  },
+  {
+    value: "김해시",
+    label: (
+      <span>
+        <b>김해시</b>
+        <small>Gimhae</small>
+      </span>
+    ),
+  },
+  {
+    value: "밀양시",
+    label: (
+      <span>
+        <b>밀양시</b>
+        <small>Miryang</small>
+      </span>
+    ),
+  },
+  {
+    value: "거제시",
+    label: (
+      <span>
+        <b>거제시</b>
+        <small>Geoje</small>
+      </span>
+    ),
+  },
+  {
+    value: "양산시",
+    label: (
+      <span>
+        <b>양산시</b>
+        <small>Yangsan</small>
+      </span>
+    ),
+  },
+  {
+    value: "제주시",
+    label: (
+      <span>
+        <b>제주시</b>
+        <small>Jeju</small>
+      </span>
+    ),
+  },
+  {
+    value: "서귀포시",
+    label: (
+      <span>
+        <b>서귀포시</b>
+        <small>Seogwipo</small>
       </span>
     ),
   },
